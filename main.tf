425f56d30676a229d7246314518109a5b68aacd4
eclarkialr <eli.clark@ialr.org>
2026-06-08 10:33:47 -0400

    Recreate blog security group

--- main.tf
+++ main.tf
@@ -19,23 +19,26 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "HelloWorld"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "6.0.0"
+  name = "blog"
+  
+  vpc_id = data.aws_vpc.default.id
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
