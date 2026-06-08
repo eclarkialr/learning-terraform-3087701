7c533ab12a5572ff35cd0fc30ce2ead1411a0e75
eclarkialr <eli.clark@ialr.org>
2026-06-08 11:31:07 -0400

    load balancer #2 attempt

--- main.tf
+++ main.tf
@@ -79,9 +79,8 @@ module "blog_alb" {
   tags = {
     Environment = "dev"
   }
-}
 
-resource "aws_lb_target_group" "blog" {
+  resource "aws_lb_target_group" "blog" {
   name     = "blog"
   port     = 80
   protocol = "HTTP"
@@ -92,4 +91,5 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_group_arn = aws_lb_target_group.blog.arn
   target_id        = aws_instance.blog.id
   port             = 80
+}
 }
