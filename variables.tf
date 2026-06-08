599c36ff4e92a4e6221b04291f7bdb0a289e5373
eclarkialr <eli.clark@ialr.org>
2026-06-08 14:24:46 -0400

    take two bro

--- variables.tf
+++ variables.tf
@@ -5,14 +5,15 @@ variable "instance_type" {
 
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
-  type = object ({
+
+  type    = object ({
     name  = string
     owner = string
   })
 
   default = {
-    name  = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-    owner = ["979382823631"] # Bitnami
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
