566c51818e3a7ed1022f623cc8080cc03b5aa135
eclarkialr <eli.clark@ialr.org>
2026-06-08 12:03:01 -0400

    04_2 answer pasted

--- main.tf
+++ main.tf
@@ -20,14 +20,61 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
     Environment = "dev"
   }
 }
@@ -38,69 +85,8 @@ module "blog_sg" {
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
-
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-
-  security_groups = [module.blog_sg.security_group_id]
-
-listeners = {
-  blog-http = {
-    port     = 80
-    protocol = "HTTP"
-
-    forward = {
-      target_groups = [
-        {
-          target_group_arn = aws_lb_target_group.blog.arn
-          weight           = 1
-        }
-      ]
-    }
-  }
-}
-  tags = {
-    Environment = "dev"
-  }
-}
-
-resource "aws_lb_target_group" "blog" {
-  name     = "blog"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
-}
-
-module "blog_autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.2"
-
-  name = "blog"
-
-  min_size = 1
-  max_size = 1
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-
-  launch_template_name = "blog"
-  security_groups    = [module.blog_sg.security_group_id]
-  instance_type      = var.instance_type
-  image_id           = data.aws_ami.app_ami.id
-  
-  traffic_source_attachments = {
-    blog-alb = {
-      traffic_source_identifier = aws_lb_target_group.blog.arn
-    }
-  }
 }
