83876ca225c05297fc6573494efb38cf446879b1
eclarkialr <eli.clark@ialr.org>
2026-06-09 11:40:54 -0400

    added launch template #2 attempt

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -43,7 +43,10 @@ module "blog_autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
-  launch_template_name = "blog"
+launch_template = {
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+}
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
