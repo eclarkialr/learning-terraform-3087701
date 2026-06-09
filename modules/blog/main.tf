57ebc664b49bc23fff5a80197bb7823534b93951
eclarkialr <eli.clark@ialr.org>
2026-06-09 14:25:15 -0400

    create ec2 instance

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -14,6 +14,15 @@ data "aws_ami" "app_ami" {
   owners = [var.ami_filter.owner]
 }
 
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
 
 module "blog_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
